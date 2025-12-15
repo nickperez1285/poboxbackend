380b855402d473bc9d2a8193efd80dacb092872d
nick perez <nickperez1285@gmail.com>
2025-12-14 21:13:43 -0800

    please god

---
+++ backend/server.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const checkoutRoutes = require('./routes/checkoutRoutes');
+const customerRoutes = require('./routes/customerRoutes');
+const priceRoutes = require('./routes/priceRoutes');
+const couponRoutes = require('./routes/couponRoutes');
+const subscriptionRoutes = require('./routes/subscriptionRoutes');
+const webhookRoutes = require('./routes/webhookRoutes');
+const productRoutes = require('./routes/productRoutes');
+const authRoutes = require("./routes/auth");
+const cors = require('cors');
+const connectDB = require("./config/db");
+// removed for vercel 
+// const dotenv = require('dotenv');
+// dotenv.config();
+
+const app = express();
+
+
+
+
+app.use(
+  "/api/webhook",
+  require("./routes/webhookRoutes")
+);
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+
+
+
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3000",
+      process.env.BASE_URL
+    ],
+    credentials: true,
+  })
+);
+
+//connect to mongodb
+
+connectDB();
+
+app.use('/api', customerRoutes);
+app.use('/api', priceRoutes);
+app.use('/api', subscriptionRoutes);
+// app.use('/api', webhookRoutes);
+app.use('/api', checkoutRoutes);
+app.use('/api', couponRoutes)
+app.use('/api', productRoutes)
+app.use("/api/auth", authRoutes);
+
+
+const PORT = process.env.PORT || 3001;
+
+if (require.main === module) {
+  app.listen(PORT, () => console.log(`Server running on ${PORT}`));
+}
+
+module.exports = app;
