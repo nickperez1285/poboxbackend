d02b38579aff4967619e679eafe0667cfd507626
nick perez <nickperez1285@gmail.com>
2025-12-14 22:29:39 -0800

    prease 23

--- backend/config/db.js
+++ backend/config/db.js
@@ -10,7 +10,7 @@ const connectDB = async () => {
   if (cached.conn) return cached.conn;
 
   if (!cached.promise) {
-    cached.promise = mongoose.connect(process.env.MONGO_URI, {
+    cached.promise = mongoose.connect(process.env.MONGODB_URI, {
       bufferCommands: false,
     });
   }
