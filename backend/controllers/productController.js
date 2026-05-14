3a29406711dfcd3fe27ebf31076884809d18a3c1
Nick <nickperez1285@gmail.com>
2026-05-14 14:26:44 -0700

    updates

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,13 +1,16 @@
-const stripe = require('../config/stripeConfig');
+const stripe = require("../config/stripeConfig");
 
 exports.getProductDetails = async (req, res) => {
-    try {
-        // Fetch product details from Stripe
-        const product = await stripe.prices.retrieve('price_1SbpjxILNRQzIFDVtfcg2EHK'); // Replace with your actual price ID
-        res.json({ success: true, product });
-    } catch (error) {
-        console.error('Error fetching product details:', error);
-        res.status(500).json({ success: false, message: 'Failed to fetch product details' });
-    }
+  try {
+    // Fetch product details from Stripe
+    const product = await stripe.prices.retrieve(
+      "price_1TX63SILNRQzIFDVJSeDNZxa",
+    ); // Replace with your actual price ID
+    res.json({ success: true, product });
+  } catch (error) {
+    console.error("Error fetching product details:", error);
+    res
+      .status(500)
+      .json({ success: false, message: "Failed to fetch product details" });
+  }
 };
-
